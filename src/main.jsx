dee8c608feeb582c177c69b7f8bf317668ceda69
Ryan E Davis <redavi19@asu.edu>
2026-08-26 17:57:48 -0400

    Load Spectrum Hero brand sync

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App.jsx'
 import { CartProvider } from './cartState'
+import BrandSync from './BrandSync.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CartProvider>
+      <BrandSync />
       <App />
     </CartProvider>
   </StrictMode>,
